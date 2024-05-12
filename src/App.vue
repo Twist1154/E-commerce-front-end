854789e39248afb2182691463407593c629c5c20
Twist1154n <rethabile1154n@gmail.com>
2024-05-12 22:40:00 +0200

    intial creation of the vue front-end page with products page product details page shopping cart page ,also included is pictures for the products page

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>African Arts & Craft</h1>
+  <div class="page-wrap">
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
---
+++ src/pages/ProductDetailPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="img-wrap">
+    <img :src="product.imageName" />
+  </div>
+  <div class="product-details">
+    <h1>{{ product.name }}</h1>
+    <div class="price-wrap"> <!-- New div wrapper for the price -->
+      <h3 class="price">{{ product.price }}</h3>
+    </div>
+    <button class="add-to-cart">Add to cart</button>
+  </div>
+</template>
+
+<script>
+import { products } from '../temp-data';
+
+export default {
+  name: "ProductDetailPage",
+  data() {
+    return {
+      product: products.find(product => product.id === this.$route.params.productId),
+    }
+  }
+}
+</script>
